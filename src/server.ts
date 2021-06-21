b967e8a38abc78ec25bfb1cb5144795a390540f3
rscoates <richard.coates@bytelondon.com>
2021-06-21 14:56:49 +0100

    Convert to TypeScript

---
+++ src/server.ts
@@ -0,0 +1,81 @@
+import config from './config'
+import * as fs from 'fs'
+import parse from 'csv-parse'
+import async from 'async'
+import list from './list'
+
+const inputFile = 'members.csv'
+const asyncTasks = []
+
+const parser = parse({ delimiter: ',' }, function (err, data) {
+  const membersToAdd = data[0].filter(function (member) {
+    return member.length
+  })
+
+  const numMembers = membersToAdd.length
+  if (numMembers > 5000) {
+    console.log('A maximum of 5000 members can be added to a list. Please reduce the number of members and try again.')
+    return false
+  }
+
+  const allMembers = membersToAdd.slice(0).map(function (handle) {
+    return handle.replace('@', '').trim()
+  })
+
+  const membersToAddArr = []
+  while (membersToAdd.length) {
+    const members = membersToAdd
+      .splice(0, 51)
+      .map(function (handle) {
+        return handle.replace('@', '').trim()
+      })
+      .join(',')
+    membersToAddArr.push(members)
+  }
+
+  membersToAddArr.forEach(function (members) {
+    asyncTasks.push(function (callback) {
+      setTimeout(function () {
+        const membersAdded = function (err) {
+          if (err) {
+            return callback && callback(err)
+          }
+          return callback && callback()
+        }
+        list.addMembersToList(config.userDetails.listName, config.userDetails.yourHandle, members, membersAdded)
+      }, 5000)
+    })
+  })
+
+  async.series(asyncTasks, function (err) {
+    if (err) {
+      console.log(err)
+      return false
+    }
+
+    /**
+     * Fetching users that have been added to the list, to check if any users weren't added.
+     * Outputs the left-out users to console.
+     **/
+
+    const membersFetched = function (err, memberHandles) {
+      if (err) {
+        console.log(err)
+        return false
+      }
+
+      const omittedUsers = allMembers.filter((x) => memberHandles.indexOf(x) < 0)
+      console.log(
+        "The following users weren't added to the list, as they were protected accounts, or invalid ones. :(  "
+      )
+      for (const i in omittedUsers) {
+        console.log('https://twitter.com/' + omittedUsers[i])
+      }
+      return
+    }
+
+    list.getMembers(config.userDetails.listName, config.userDetails.yourHandle, numMembers, membersFetched)
+  })
+})
+
+fs.createReadStream(inputFile).pipe(parser)
